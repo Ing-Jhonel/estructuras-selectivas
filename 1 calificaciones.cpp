5342c82391064003ba2b7aee2a8a49ad40e8e6e5
Jhonel <jgapazap@unjbg.edu.pe>
2024-06-13 22:25:14 -0500

    1 modificacion: expecificacion del problema a resolver | Dia 1: Agregar ejercicio 1 calificaciones

--- 1 calificaciones.cpp
+++ 1 calificaciones.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 using namespace std;
+// Determinar si un alumno aprueba o reprueba un curso, sabiendo que aprobara si su
+// promedio de tres calificaciones es mayor o igual a 70; reprueba en caso contrario.
+// ENTRADA|| calificacion1: primer calificacion calificacion2: segunda calificacion calificacion3: tercera calificacion
+// SALIDA|| "Aprobado" "Reprobado"
 
 int main (){
 	
-	float c1, c2, c3, promedio;
+	float calificacion1, calificacion2, calificacion3, promediodecalificaciones123;
 	cout << "Introduce la primera calificacion: ";
-	cin >> c1;
+	cin >> calificacion1;
 	cout << "Introduce la segunda calificacion: ";
-	cin >> c2;
+	cin >> calificacion2;
 	cout << "Introduce la tercera calificacion: ";
-	cin >> c3;
-	promedio = (c1+c2+c3)/3;
-	if (promedio >= 70){
+	cin >> calificacion3;
+	promediodecalificaciones123 = (calificacion1+calificacion2+calificacion3)/3;
+	if (promediodecalificaciones123 >= 70){
 		cout << "Aprobado.";
 	} else {
 		cout << "Reprobado.";
 	}
 	return 0;
-
 }
