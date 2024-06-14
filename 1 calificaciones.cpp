ff6da947a88a5974a7a2597aeb6b5e74db048fbb
Jhonel <jgapazap@unjbg.edu.pe>
2024-06-14 17:54:59 -0500

    2 modificacion: condicione que la nota debe ser positiva || Dia 1: agregue ejercicio 1 calificaciones

--- 1 calificaciones.cpp
+++ 1 calificaciones.cpp
@@ -8,17 +8,42 @@ using namespace std;
 int main (){
 	
 	float calificacion1, calificacion2, calificacion3, promediodecalificaciones123;
+	cout << "AVERIGUA SI ESTAS APROBADO O DESAPROBADO" << endl << endl;
 	cout << "Introduce la primera calificacion: ";
 	cin >> calificacion1;
+	while(calificacion1<0 ){
+		cout << endl;
+		cout << "No valido. Intente de nuevo." << endl;
+		cout << "Introduce la primera calificacion: ";
+		cin >> calificacion1;
+		cout << endl;
+	}
 	cout << "Introduce la segunda calificacion: ";
 	cin >> calificacion2;
+	while(calificacion2<0 ){
+		cout << endl;
+		cout << "No valido. Intente de nuevo."<< endl;
+		cout << "Introduce la segunda calificacion: ";
+		cin >> calificacion2;
+		cout << endl;
+	}
 	cout << "Introduce la tercera calificacion: ";
 	cin >> calificacion3;
-	promediodecalificaciones123 = (calificacion1+calificacion2+calificacion3)/3;
-	if (promediodecalificaciones123 >= 70){
-		cout << "Aprobado.";
-	} else {
-		cout << "Reprobado.";
+	while(calificacion3<0 ){
+		cout << endl;
+		cout << "No valido. Intente de nuevo." << endl;
+		cout << "Introduce la tercera calificacion: ";
+		cin >> calificacion3;
+		cout << endl;
 	}
+	promediodecalificaciones123 = (calificacion1+calificacion2+calificacion3)/3;
+	string estatus;
+	cout << endl;
+	if (promediodecalificaciones123 >= 70){
+		estatus = "aprobado.";
+	} else {
+		estatus = "reprobado.";
+	}
+	cout << "Usted esta " << estatus << endl;
 	return 0;
 }
--- 3 camisas descuento.cpp
+++ 3 camisas descuento.cpp
@@ -1,24 +1,50 @@
 #include <iostream>
 using namespace std;
+// Calcular el total a pagar por la compra de camisas. Si se compran tres camisas o más se aplica un descuento del }
+// 20% sobre el total de la compra y si son menos de tres camisas un descuento del 10%
+// ENTRADA|| camisas: numero de camisas a llevar  valor_compra: valor de la compra original
+// SALIDA|| valor_final: valor final a pagar despues del descuento
 
 int main (){
 	
-	double camisas, valor_compra, valor_final, descuento;
-	cout << "¿Cuantas camisas va a llevar?: ";
+	double camisas, valor_compra=0, valor_final, descuento, porcentaje, costocamisa;
+	string respuesta;
+	cout << "Indique cuantas camisas va a llevar: ";
 	cin >> camisas;
-	cout << "Introduzca el valor de la compra: ";
-	cin >> valor_compra;
-	if (camisas >= 3){
-		descuento = valor_compra*0.2;
-		valor_final = valor_compra - descuento;
-		cout << "El valor final de su compra es: " << valor_final << endl;
-	} else {
-		descuento = valor_compra*0.1;
-		valor_final = valor_compra - descuento;
-		cout << "El valor final de su compra es: " << valor_final << endl;
+	cout << endl;
+	cout << "¿Las " << camisas << " camisas cuestan lo mismo?(si/no): "; 
+	cin >> respuesta;
+	cout << endl;
+	if(respuesta == "si" || respuesta == "no"){
+		do{
+			cout << "Indique si"
+		}
 	}
-
-	cout << "¡¡GRACIAS POR SU COMPRA!!" << endl;
-	
+	if(respuesta == "si"){
+		cout << "Indique el costo de una camisa: ";
+		cin >> costocamisa;
+		valor_compra = costocamisa*camisas;
+	} else {
+		cout << "Indique el costo de cada camisa: " << endl << endl;
+		for(int i=1; i<= camisas; i++){
+			cout << i << ". Camisa: ";
+			cin >> costocamisa;
+			valor_compra = valor_compra + costocamisa;
+		}
+	} 
+	cout << endl;
+	cout << "El valor inicial de todo es: " << valor_compra;
+	cout << endl;
+	if (camisas >= 3){
+		porcentaje = 0.2;
+	} else {
+		porcentaje = 0.1;
+	}
+	descuento = valor_compra*porcentaje;
+	cout << endl;
+	cout << "--------- ¡¡SE LE APLICO UN DESCUENTO DE " << porcentaje*100 << "%!! ---------" << endl << endl;
+	valor_final = valor_compra - descuento;
+	cout << "El valor final de su compra es: " << valor_final << endl << endl;
+	cout << "-------------- ¡¡GRACIAS POR SU COMPRA!! ---------------" << endl;
 	return 0;
 }
