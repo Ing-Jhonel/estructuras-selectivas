0541037425b46898a4ca1213679f9e6b9ae9469a
Jhonel <jgapazap@unjbg.edu.pe>
2024-06-16 09:53:59 -0500

    3 modificacion: agregue condiciones al sexo || Dia 3: agregue ejercicio 5 num pulsaciones

--- 5 num pulsaciones.cpp
+++ 5 num pulsaciones.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
 using namespace std;
-
+// Calcular el número de pulsaciones que debe tener una persona por cada 10 segundos de ejercicio aeróbico; 
+// la fórmula que se aplica es:
+// cuando el sexo es femenino : num. pulsaciones = (220 - edad)/10
+// y si el sexo es masculino : num. pulsaciones = (210 - edad)/10
+// ENTRADA|| edad: edad del usuario  sexo: sexo del usuario
+// SALIDA|| numero_pulsaciones: numero de pulsaciones del usuario
 int main(){
 	
-	double n_pulsaciones, edad, masculino, femenino; string sexo;
+	double numero_pulsaciones, edad; 
+	string sexo;
+	cout << "CALCULADORA DE PULSACIONES POR 10 SEGUNDOS DE EJERCICIO AEROBICO" << endl;
+	cout << "****************************************************************" << endl << endl;
 	cout << "Ingrese su edad: ";
 	cin >> edad;
-	cout << "¿De que sexo es usted?: ";
+	cout << endl;
+	cout << "Ingrese su sexo (masculino/femenino): ";
 	cin >> sexo;
-	if ( sexo == "masculino"){
-		n_pulsaciones = (210 - edad)/10;
-		cout << "Su numero de pulsaciones por cada 10 segundos de ejercicio aerobico es: " << n_pulsaciones << endl;
-	} if (sexo == "femenino"){
-		n_pulsaciones = (220 - edad)/10;
-		cout << "Su numero de pulsaciones por cada 10 segundos de ejercicio aerobico es: " << n_pulsaciones << endl;
-	} else {
-		cout << "Sexo no identificado.";
+	while(sexo != "masculino" && sexo != "femenino"){
+		cout << endl;
+		cout << "No valido." << endl;
+		cout << "Ingrese su sexo (masculino/femenino): ";
+		cin >> sexo;
 	}
+	if ( sexo == "masculino"){
+		numero_pulsaciones = (210 - edad)/10;
+	} else {
+		numero_pulsaciones = (220 - edad)/10;
+	}
+	cout << "----------------------------------------------------------------------------" << endl;
+	cout << "Su numero de pulsaciones por cada 10 segundos de ejercicio aerobico es: " << numero_pulsaciones << endl;
 	return 0;
 }
